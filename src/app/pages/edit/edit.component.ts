4ead63225b7424baaf4944fd2dea471c7f93800e
Jimmy Martinez <68252026+MegaGone@users.noreply.github.com>
2021-02-20 18:31:20 -0600

    Form validation

--- src/app/pages/edit/edit.component.ts
+++ src/app/pages/edit/edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import Swal from 'sweetalert2';
 import { HeroModel } from '../../models/heros';
 @Component({
   selector: 'app-edit',
@@ -14,9 +16,31 @@ export class EditComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  
-  Save(){
-    console.log(this.hero);
+
+  Save(f: NgForm) {
+
+    // Invalid form
+    if(f.invalid){
+      return Object.values( f.controls ).forEach(control => {
+        control.markAsTouched();
+      });
+
+    // Valid form
+    }else{
+
+      const name = this.hero.name;
+      Swal.fire({
+        title: 'Success',
+        icon: 'success',
+        text: `${name} add successfully`,
+        showConfirmButton: false,
+        timer: 1500
+      });
+
+      f.reset();
+    }
+
+
   }
 
 }
